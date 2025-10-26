c03b2d4c29a153916c50fce2d12d7e370d03244d
Cristian Andres Rojas Vargas <crisanrova@gmail.com>
2025-10-26 18:56:27 -0500

    Hola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Practica';
+  title = 'Practica cristian';
+  subtitulo = 'Mi primer proyecto con Angular';
+  parrafo = 'Este es un proyecto de prueba para aprender Angular.';
 }
